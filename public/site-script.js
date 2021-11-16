342360c1be2bb43e4874f52698a51c61103d152d
Sakari Woods <woodss9@wwu.edu>
2021-11-16 21:18:00 +0000

    local subscan working, drawing d3 map of connected devices on card.

--- public/site-script.js
+++ public/site-script.js
@@ -24,6 +24,15 @@ request.onload = function(){
 
 	// Activate zooming map.
 	zoomSys = setInterval(zoomFunc, 200);
+
+	// Scan local network map.
+	scanLocalNetwork();
+
+	// Populate network card with found connections after 3 seconds.
+	setTimeout(function(){
+		// Render out a visualization of the user's port-80 visible local network.
+		visualizeNetwork();
+	},3000);
 }
 
 function zoomFunc() {
@@ -119,3 +128,106 @@ $(document).ready(function() {
 
 })
 
+// Hold the starting and ending times of the scans.
+startScan = [];
+endScan = [];
+foundConnections = [];
+connectionCount = 0;
+
+// Checks the local sub-network of the user from 0-255, and returns the ip addresses with an exposed port 80.
+// Realistically, this will be the user's own computer, the router they're connected to and potentially a printer.
+async function fetchAsync (url,i) {
+	try{
+		let response = await fetch(url);
+	}
+	catch(err){
+		endScan[i] = Date.now();
+		if((endScan[i] - startScan[i]) < 2000){
+			foundConnections[connectionCount] = url;
+			connectionCount++;
+		}
+	}
+}
+
+function scanLocalNetwork(){
+	for(let i=0;i<255;i++){
+		startScan[i] = Date.now();
+		fetchAsync("//192.168.1."+i,i);
+	}
+}
+function visualizeNetwork(){
+var json = {
+	"nodes": [],
+	"links": []
+};
+
+var i = 0;
+var routerConnection = 0;
+for(i=0;i<foundConnections.length;i++){
+	console.log("Adding connection "+foundConnections[i]);
+	// If the connection ends in .1, we have found the router.
+	if(foundConnections[i].endsWith('.1')){
+		json.nodes[i] = {"id": i, "name": foundConnections[i].substring(2), "image": "https://win98icons.alexmeub.com/icons/png/modem-5.png"};
+		routerConnection = i;
+	}
+	else{
+		json.nodes[i] = {"id": i, "name": foundConnections[i].substring(2), "image": "https://win98icons.alexmeub.com/icons/png/computer-5.png"};
+	}
+}
+
+// Now link the found connections.
+for(i=0;i<foundConnections.length;i++){
+	json.links[i] = {"source":routerConnection, "target": i};
+}
+
+var width = 500,
+    height = 200
+
+var svg = d3.select("#NetworkVisual").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var force = d3.layout.force()
+    .gravity(0.05)
+    .distance(100)
+    .charge(-100)
+    .size([width, height]);
+
+// Perform the render.
+  force
+      .nodes(json.nodes)
+      .links(json.links)
+      .start();
+
+  var link = svg.selectAll(".link")
+      .data(json.links)
+    .enter().append("line")
+      .attr("class", "link");
+
+  var node = svg.selectAll(".node")
+      .data(json.nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .call(force.drag);
+
+  node.append("image")
+      .attr("xlink:href", function(d) { return d.image; })
+      .attr("x", -8)
+      .attr("y", -8)
+      .attr("width", 16)
+      .attr("height", 16);
+
+  node.append("text")
+      .attr("dx", 12)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.name });
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+}

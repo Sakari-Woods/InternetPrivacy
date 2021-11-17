0fb8bbc72472b309470cae3e4e2169f925486ec8
Sakari Woods <woodss9@wwu.edu>
2021-11-17 01:51:55 +0000

    Added testing container, commented out Github Action for now, need to simulate API and quering dynamic from main web container.

--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,8 @@
 var request = require('supertest');
-var app = require('../app').app;
+var app = require('/app').app;
 
-describe('Connect to the website', function(){
-	it("request json site content", function(done){
-		request(app)
-			.get("/sitecontent")
-			.set('Accept', 'application/json')
-			.expect('Content-Type', /json/)
-			.expect(200, {result : 4.6})
-			.end(function (err) {
-				if(err){
-					return done(err);
-				}
-				return done();
-			})
-	});
+request = request('http://localhost:80');
+
+request.get('/').expect(200, function(err){
+	console.log(err);
 });
